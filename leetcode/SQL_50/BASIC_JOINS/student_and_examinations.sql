a01d39c2f651780ccbc0503916071b7916c95afb
Matthew Dembinski <matthewdembinski@gmail.com>
2024-02-02 22:03:43 -0700

    ADD: SQL practice problems.

---
+++ leetcode/SQL_50/BASIC_JOINS/student_and_examinations.sql
@@ -0,0 +1,5 @@
+-- Write a solution to find the number of times each student attended each exam.
+
+-- Return the result table ordered by student_id and subject_name.
+
+-- The result format is in the following example.
